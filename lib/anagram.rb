7a2312c3366539034df6bda3ff7b9d459cae5ccf
Laura Radville <laura.radville@gmail.com>
2019-07-25 19:31:03 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    
+  end
+  
+  def match(anagrams)
+    anagrams.map do |anagram|
+      anagram.split("") == word.split("")
+    end
+  end
+  
+end
